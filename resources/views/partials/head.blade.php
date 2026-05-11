a31fbeed82d9c057cb6a2ac4bab7fa7f0b932a14
Jiordi Viera <jiordikengne@gmail.com>
2026-05-11 14:46:11 +0100

    Add analytics and Algolia verification

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,3 +12,6 @@
 
 @vite('resources/css/app.css')
 @vite('resources/js/app.js')
+
+<script defer src="https://analytics.jiordiviera.me/script.js" data-website-id="04167809-86e7-4be3-b0ca-72e2b392ee52"></script>
+<meta name="algolia-site-verification"  content="3F0311C1507C6863" />
